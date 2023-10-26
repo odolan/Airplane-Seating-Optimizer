ab97687ff1bf86ec1caba389aff0153b77e8c887
Owen Dolan <odolan27@gmail.com>
2023-10-26 14:41:40 -0400

    updates

--- seater.py
+++ seater.py
@@ -41,5 +41,20 @@ _______    ________
 
 """
 
+# given an indivudals seat and speed, and seating arrangement
+# returns the time it takes the individual to travel straight to their seat
+def time_to_seat(seat, speed, seating_arrangement):
+    pass
+
 # takes a list of people with seating arrangement
-def seater(): 
+def seater(passengers): 
+    time = 0
+    seating_arrangement = [[]]
+    
+    # turn the list of passengers into a queue 
+
+    # time how long it takes the first person to enter the plane
+
+    # and the second ... and third
+
+    pass
