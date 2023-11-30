56e3ed16a44aa489ec5e028698ce7a23f9d0bed1
Owen Dolan <odolan27@gmail.com>
2023-11-30 11:57:33 -0500

    tests

--- genetic.py
+++ genetic.py
@@ -7,6 +7,7 @@ from copy import deepcopy
 from tqdm import tqdm
 import statistics
 import matplotlib.pyplot as plt
+import numpy as np
 
 # runs a tournament on the supplied population that will output the given number of survivor
 # a tournament is a random pairing of two citizens from the population, and the one with the lowest score (best seating)
@@ -27,7 +28,8 @@ avg_scores = []
 
 # runs genetic algorithm, returns the initial efficient solution among the random population
 # and the final, most efficient solution among the random population
-def genetic(generations, rows, cols, population_size, num_to_replace, num_to_mutate):
+def genetic(generations, cols, rows, population_size, num_to_replace, num_to_mutate):
+
     passenger_master_list = []
     population = []
 
@@ -41,15 +43,6 @@ def genetic(generations, rows, cols, population_size, num_to_replace, num_to_mut
     print("BUILDING INITIAL POPULATION...")
     for i in tqdm(range(population_size)):
         population.append(Citizen(rows, cols, deepcopy(passenger_master_list)))
-
-    # EXTRACT THE INTIAL MINIMUM FROM THE RANDOMLY GENERATED POPULATION
-    min_score = 9999
-    initial_min_score_citizen = None
-
-    for citizen in population:
-        if citizen.score < min_score:
-            min_score = citizen.score
-            initial_min_score_citizen = citizen
 
     # update over generations
     for generation in tqdm(range(generations)):
@@ -83,57 +76,89 @@ def genetic(generations, rows, cols, population_size, num_to_replace, num_to_mut
         min_scores.append(min(scores))
         avg_scores.append(statistics.mean(scores))
     
-    plt.ylabel("Generation Score")
-    plt.xlabel("Generation")
-    x = range(1, generations + 1)
-    plt.plot(x, avg_scores, label="Average")
-    plt.plot(x, min_scores, label="Minimum")
-    plt.legend()
-    plt.savefig("ScoreGraph.png")
+    # plt.ylabel("Generation Score")
+    # plt.xlabel("Generation")
+    # x = range(1, generations + 1)
+    # plt.plot(x, avg_scores, label="Average")
+    # plt.plot(x, min_scores, label="Minimum")
+    # plt.legend()
+    # plt.savefig("ScoreGraph.png")
 
     min_score = 9999
     final_min_score_citizen = None
 
+    # recover best boarder ordering from the population
     for citizen in population:
         if citizen.score < min_score:
             min_score = citizen.score
             final_min_score_citizen = citizen
 
-    return (initial_min_score_citizen, final_min_score_citizen)
+    return (final_min_score_citizen)
 
 
-# GENETIC FUNC ARGS: generations, rows, cols, pop_size, num to replace, num to mutate
-initial, final = genetic(350, 10, 3, 600, 20, 200)
+def get_slowness(ordering):
+    return [person.delay_prob for person in ordering]
 
-plt.clf()
-plt.xlabel("Nth Passenger in Seating Order")
-plt.ylabel("Row of Seat Assignment For nth Passenger")
+def get_row_positon(ordering):
+    return [person.seat_assignment[0] for person in ordering]
 
-x = range(len(initial.specific_ordering))
-initial_row_data = []
-final_row_data = []
+data_x = np.array([]) 
+data_y = np.array([])
+for _ in range(150):
+    print(_)
+    ordering = genetic(generations=40, rows=20, cols=3, population_size=100, num_to_replace=15, num_to_mutate=20).specific_ordering
+    slowness_vals = get_slowness(ordering)
+    data_y = np.append(data_y, slowness_vals, axis=0)
+    row_vals = get_row_positon(np.array(ordering))
+    data_x = np.append(data_x, row_vals, axis=0)
 
-for i in range(len(initial.specific_ordering)):
-    initial_row_data.append(initial.specific_ordering[i].seat_assignment[0])
-    final_row_data.append(final.specific_ordering[i].seat_assignment[0])
+x_flat = data_x.flatten()
+y_flat = data_y.flatten()
 
-plt.scatter(x, initial_row_data, label="Initial")
-plt.scatter(x, final_row_data, label="Final")
+print(x_flat.shape)
+print(y_flat.shape)
+
+
+np.savetxt('x_data.txt', x_flat, delimiter=',', header='X Data')
+np.savetxt('y_data.txt', y_flat, delimiter=',', header='Y Data')
+
+
+plt.scatter(x_flat, y_flat, c='blue', marker='o', label='Data Points', alpha=0.02)
+plt.xlabel('Position in Line')
+plt.ylabel('Slowness')
+plt.title('Scatter Plot of Position vs Slowness')
 plt.legend()
-plt.savefig("RowGraph.png")
+plt.show()
 
-plt.clf()
-plt.xlabel("Nth Passenger in Seating Order")
-plt.ylabel("Delay Probability of nth Passenger")
-initial_row_data = []
-final_row_data = []
+# plt.clf()
+# plt.xlabel("Nth Passenger in Seating Order")
+# plt.ylabel("Row of Seat Assignment For nth Passenger")
 
-for i in range(len(initial.specific_ordering)):
-    initial_row_data.append(initial.specific_ordering[i].delay_prob)
-    final_row_data.append(final.specific_ordering[i].delay_prob)
+# x = range(len(initial.specific_ordering))
+# initial_row_data = []
+# final_row_data = []
 
-plt.scatter(x, initial_row_data, label="Initial")
-plt.scatter(x, final_row_data, label="Final")
-plt.legend()
-plt.savefig("DelayGraph.png")
+# for i in range(len(initial.specific_ordering)):
+#     initial_row_data.append(initial.specific_ordering[i].seat_assignment[0])
+#     final_row_data.append(final.specific_ordering[i].seat_assignment[0])
+
+# plt.scatter(x, initial_row_data, label="Initial")
+# plt.scatter(x, final_row_data, label="Final")
+# plt.legend()
+# plt.savefig("RowGraph.png")
+
+# plt.clf()
+# plt.xlabel("Nth Passenger in Seating Order")
+# plt.ylabel("Delay Probability of nth Passenger")
+# initial_row_data = []
+# final_row_data = []
+
+# for i in range(len(initial.specific_ordering)):
+#     initial_row_data.append(initial.specific_ordering[i].delay_prob)
+#     final_row_data.append(final.specific_ordering[i].delay_prob)
+
+# plt.scatter(x, initial_row_data, label="Initial")
+# plt.scatter(x, final_row_data, label="Final")
+# plt.legend()
+# plt.savefig("DelayGraph.png")
 
