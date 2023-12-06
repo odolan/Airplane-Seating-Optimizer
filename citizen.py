9a6e6aa50f993c689540b57a57751837150805af
mrwrench24 <jake.squatrito@icloud.com>
2023-12-06 13:14:57 -0500

    documenting citizen

--- citizen.py
+++ citizen.py
@@ -1,5 +1,4 @@
 from airplane import Airplane
-from person import Person
 from copy import deepcopy
 import statistics
 import random
@@ -7,9 +6,13 @@ import random
 # Represents a citizen in the population (one single simulation that has a tick count, specific passenger ordering,
 # challenged by other citizens to survive, merge/mutates)
 class Citizen():
+    # the unique order in which people are sat on airplanes for this citizen
     specific_ordering = []
+    # the airplane used to run simulations - reset (rebuild) after running a simulation
     airplane = None
+    # number of ticks (takes average) to run previous simulation
     score = 999999
+    # dimensions to be used for this citizen (used to rebuild airplanes)
     num_rows = 0
     num_cols = 0
 
@@ -51,23 +54,27 @@ class Citizen():
                 break
 
     # returns a citizen that is the "offspring" of this and the supplied other citizen
-    # supplied list must already be a deep copy - will be mutated!
+    # NOTE: supplied "master list" must already be a deep copy - will be mutated!
     def reproduce(self, other_citizen, passenger_master_list):
         crossover_list = [None] * len(self.specific_ordering)
         random.shuffle(passenger_master_list)
 
-        # List of tuples of seat arrangments that have been sat - easy way to check for citizen equality/presence due to the deepcopying that has been done,
-        # who knows what kind of errors would pop up
+        # List of tuples of seat arrangments that have been sat - used to check for passenger equality/presence in a list
         used_seatings = []
+
+        # Citizen that will be the offspring (list updated at end of method)
         new_citizen = Citizen(self.num_rows, self.num_cols, passenger_master_list)
 
-        # initially tries to assign orderings from parents specifically
+        # Will make n attempts to draw each of the n passengers using "information" from each passenger
         for i in range(int(len(self.specific_ordering))):
             rand_index = random.randint(0, len(self.specific_ordering) - 1)
             try_count = 0
+
+            # 50/50 chance, pull from parent1 (self)
             if i % 2 == 0:
+                # random passenger, will see if we can write this to the list
                 rand_passenger = self.specific_ordering[rand_index]
-                # regenerate random passengers if this passenger (checked by the list of seat assignments added) already part of child
+                # BOOLEANS: 1. Prevent overwrites   2. Prevent duplicates   3. Don't try too much (might not be any options!)
                 while (crossover_list[rand_index] != None or rand_passenger.seat_assignment in used_seatings) and try_count < len(passenger_master_list) * 2:
                     rand_index = random.randint(0, len(self.specific_ordering) - 1)
                     rand_passenger = self.specific_ordering[rand_index]
@@ -78,12 +85,13 @@ class Citizen():
                     continue
 
                 # once a unique has been found, assign a copy to the final list, add its seat assignment to the "seen", remove it from the master list
-                # so passengers that aren't added in this way are added in the end
-
+                # so passengers that aren't added with "unique" information can be randomly assigned at the end
                 crossover_list[rand_index] = deepcopy(rand_passenger)
                 used_seatings.append(rand_passenger.seat_assignment)
                 self.remove_pass_with_assign(passenger_master_list, rand_passenger.seat_assignment)
+            # 50/50 chance, pull from parent2 (other)
             else:
+                # ** same code as above, see comments **
                 rand_passenger = other_citizen.specific_ordering[rand_index]
                 while (crossover_list[rand_index] != None or rand_passenger.seat_assignment in used_seatings) and try_count < len(passenger_master_list) * 2:
                     rand_index = random.randint(0, len(self.specific_ordering) - 1)
@@ -127,6 +135,7 @@ class Citizen():
         return result
 
     # calculates a score for this simulation by running a "seating" multiple times (since seatings can vary due to delays)
+    # and takes the avg. of seatings, assigns that number as the "score"
     def calc_score(self):
         scores = []
 
@@ -138,25 +147,9 @@ class Citizen():
         return self.score
     
     # EFFECT: Runs the necessary updates associated with changing the passenger ordering for a specific citizen
-    # (Changing airplane, updating score, etc...)
+    # (Changing airplane, updating score + running simulation, etc...)
     def update_ordering(self, new_ordering):
         self.specific_ordering = new_ordering
         # initial airplane set up for the first score calculation
         self.airplane = Airplane(self.num_rows, self.num_cols, deepcopy(self.specific_ordering))
         self.calc_score()
-
-# passengers = []
-
-# for row in range(5):
-#     for col in range(3):
-#         passengers.append(Person(random.uniform(0, 0.25), (row, col)))
-
-# citizen1 = Citizen(5, 3, passengers)
-# citizen2 = Citizen(5, 3, passengers)
-
-# print(citizen1)
-# print(citizen2)
-
-# child = citizen1.reproduce(citizen2, passengers)
-
-# print(child)
